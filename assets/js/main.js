63102947eadee24ac33e6ee4c8461bc51867cf54
andykmccoy <andy.k.mccoy@gmail.com>
2014-07-27 21:32:25 -0700

    firebase added to js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
 ;(function(){
+  var App = {};
+  App.firebase = new Firebase("https://intense-fire-661.firebaseio.com/");
+
+  App.save = function(name){
+    this.firebase.set({
+      name: name
+    }); 
+  };
   
 })();
