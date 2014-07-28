87862279254eb80a8def09d1c3e7fefa148eab43
andykmccoy <andy.k.mccoy@gmail.com>
2014-07-27 20:48:12 -0700

    live reload working

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,55 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     connect: {
       server: {
         options: {
           port: 9876,
-          open: true,
+          //open: true,
           keepalive: true
         }
       }
+    },
+
+    compass: {
+      dev: {
+        options: {
+          bundleExec: true,
+          http_path: "/",
+          cssDir: "assets/stylesheets",
+          sassDir: "assets/scss",
+          imagesDir: "assets/images",
+          javascriptsDir: "assets/js",
+          relativeAssets: true,
+          watch: true
+        }
+      }
+    },
+
+    watch: {
+      files: ['*.html', 'assets/stylesheets/**/*.css', 'assets/js/**/*.js', 'assets/images/**/*'],
+      options: { 
+        livereload: true 
+      },
+    },
+
+    concurrent: {
+      dev: ['connect', 'compass:dev', 'watch'],
+      options: {
+        logConcurrentOutput: true
+      }
     }
+
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['connect']);
+  grunt.registerTask('default', ['concurrent:dev']);
 
 };
